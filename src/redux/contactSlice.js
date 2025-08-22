13ea3dcb487bca25fe0b051791c5bf087861d902
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-08-22 17:38:41 +0300

    м
    м

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,42 +1,57 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const baseUrl = 'https://6604579b2ca9478ea17dd2fa.mockapi.io/contacts';
+axios.defaults.baseURL = 'https://68a0765b6e38a02c5818c626.mockapi.io';
+
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
-  async () => {
-    const response = await axios.get(baseUrl);
-    return response.data;
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 
 export const addContact = createAsyncThunk(
   'contacts/addContact',
-  async ({ name, number }) => {
-    const response = await axios.post(baseUrl, { name, number });
-    return response.data;
+  async ({ name, number }, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', { name, number });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
-  async (contactId) => {
-    await axios.delete(`${baseUrl}/${contactId}`);
-    return contactId;
+  async (contactId, thunkAPI) => {
+    try {
+      await axios.delete(`/contacts/${contactId}`);
+      return contactId;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
+
 
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
     items: [],
     isLoading: false,
-    error: null
+    error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
+      
       .addCase(fetchContacts.pending, (state) => {
         state.isLoading = true;
         state.error = null;
@@ -47,13 +62,35 @@ const contactsSlice = createSlice({
       })
       .addCase(fetchContacts.rejected, (state, action) => {
         state.isLoading = false;
-        state.error = action.error.message;
+        state.error = action.payload;
+      })
+
+      .addCase(addContact.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
       })
       .addCase(addContact.fulfilled, (state, action) => {
+        state.isLoading = false;
         state.items.push(action.payload);
       })
+      .addCase(addContact.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+
+      .addCase(deleteContact.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
       .addCase(deleteContact.fulfilled, (state, action) => {
-        state.items = state.items.filter(contact => contact.id !== action.payload);
+        state.isLoading = false;
+        state.items = state.items.filter(
+          (contact) => contact.id !== action.payload
+        );
+      })
+      .addCase(deleteContact.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
       });
   },
 });
